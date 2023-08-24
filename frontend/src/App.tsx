88efc81269bbd6f7af53fbcb4ab4baf8f1fa9982
samano2j <jemsamano@yahoo.com>
2023-08-24 00:00:19 -0600

    added the pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import { FC } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import Universe from './pages/Universe.tsx'
+import Privacy from './pages/Privacy.tsx'
+import Term from './pages/Term.tsx'
 const App: FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/universe" element={<Universe />} />
+        <Route path="/privacypolicy" element={<Privacy />} />
+        <Route path="/termsofuse" element={<Term />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/Privacy.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import Header from '../components/common/Header.tsx'
+import Footer from '../components/common/Footer.tsx'
+import instagramIcon from '../assets/icons/instagram.svg'
+import twitterIcon from '../assets/icons/twitter.svg'
+import facebookIcon from '../assets/icons/facebook.svg'
+import tiktokIcon from '../assets/icons/tiktok.svg'
+import discordIcon from '../assets/icons/discord.svg'
+
+const Privacy: FC = () => {
+    return (
+      <div>
+        <Header />
+        <div className="flex justify-end items-center p-10 gap-10">
+                <img src={instagramIcon} alt="instagram" className="w-10 h-10" />
+                <img src={twitterIcon} alt="twitter" className="w-10 h-10" />
+                <img src={facebookIcon} alt="facebook" className="w-10 h-10" />
+                <img src={tiktokIcon} alt="tiktok" className="w-10 h-10" />
+                <img src={discordIcon} alt="discord" className="w-10 h-10" />
+        </div>
+        <div className='flex flex-col items-center mb-20'>
+            <h1 className="font-ldr-kaet text-4xl">PRIVACY POLICY</h1>
+            <div className='h-screen w-5/6 bg-[#F8F8E4] rounded mt-2' style={{border: "1px solid #CE8C3D"}}></div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+  
+  export default Privacy
