aa8617bfc0804312283b3c9428d2f3cabb4459f5
kazukiii <kazumatsu.dev28@gmail.com>
2023-08-25 13:58:35 -0700

    fix backend

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import multer from 'multer'
-import path from 'path'
 import {
   createProduct,
   getAllProducts,
@@ -10,17 +9,7 @@ import {
 } from '../controllers/productController'
 
 const router = express.Router()
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-  },
-})
-
-const upload = multer({ storage: storage })
+const upload = multer({ dest: 'uploads/' })
 
 router.post('/', upload.single('file'), createProduct)
 router.get('/', getAllProducts)
