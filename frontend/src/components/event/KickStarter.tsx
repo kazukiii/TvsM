b37df9465a2e59fa45561eec6796d3831e43034b
kazukiii <kazumatsu.dev28@gmail.com>
2023-08-25 14:03:17 -0700

    fix

--- frontend/src/components/event/KickStarter.tsx
+++ frontend/src/components/event/KickStarter.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react'
-import logo from '../../assets/logo.svg'
 
 const KickStarter: FC = () => {
   return (
-  <div className='my-10 flex flex-col items-center gap-8'>
-    <h1 className="font-ldr-kaet text-5xl text-center">CHECK OUT OUR <br /> KICKSTARTER CAMPAIGN!</h1>
+    <div className="my-10 flex flex-col items-center gap-8">
+      <h1 className="font-ldr-kaet text-5xl text-center">
+        CHECK OUT OUR <br /> KICKSTARTER CAMPAIGN!
+      </h1>
       <div className="w-[95%] md:w-[90%] flex gap-2 justify-center items-center">
         <div className="w-1/3 aspect-square flex flex-col gap-2">
           <div className="h-1/2 w-full border-8 border-tvsm-orange bg-panda-event bg-cover bg-center "></div>
@@ -20,7 +21,7 @@ const KickStarter: FC = () => {
           <div className="h-1/2 w-full border-8 border-tvsm-orange bg-ghost-event bg-cover  bg-center "></div>
         </div>
       </div>
-  </div>
+    </div>
   )
 }
 
