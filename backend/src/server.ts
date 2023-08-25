6fc61006673c596487b9826d35c04b2c71cc0da5
kazukiii <kazumatsu.dev28@gmail.com>
2023-08-25 01:27:25 -0700

    set public directory

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import productRoutes from './routes/productRoutes'
+import path from 'path'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, '../uploads')))
 app.use('/products', productRoutes)
 
 const PORT = 3000
